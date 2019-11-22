64b22506770be9af1ab331e8d1c11979adb56d9d
romeshstartupflux <romesh@startupflux.com>
2019-11-22 15:46:57 +0530

    Guest can register only once

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,40 +1,50 @@
 var express = require('express');
 var router = express.Router();
-const querystring = require('querystring'); 
+const querystring = require('querystring');
 
 const Quiz = require('../models/quiz')
 const Examinee = require('../models/examinee')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'QnA' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'QnA'
+  });
 });
 
 
-router.post('/register', async function(req, res, next){
+router.post('/register', async function (req, res, next) {
   const examinee = new Examinee();
-  var insertdata = await examinee.collection.insertOne({
+  if (await examinee.collection.findOne({
+      examineeName: req.body.examineeName
+    })) {
 
-    examineeName: req.body.examineeName,
-    examineeAnswer :[],
-    score : 0
+    res.send("<alert>Second Time Not Allowed.</alert>")
+  } else {
+    var insertdata = await examinee.collection.insertOne({
 
-  });
-  const firstQ = 1;
-  console.log("FIRSTQ : ",firstQ)
-  console.log("examineeName", insertdata.ops[0]._id)
-  // res.send("ok")
-  // alert("lkddhjskjsfdhjfh .........")
-  // res.send({})
-  // res.redirect('quiz/'+firstQ)
-  const query = querystring.stringify({
-    "firstQ":firstQ,
-    "examineeName":insertdata.ops[0].examineeName,
-    "examineeID": insertdata.ops[0]._id,
-  })
-  req.session.user = insertdata.ops[0].examineeName
-  req.session.userid = insertdata.ops[0]._id
-  res.redirect('quiz/'+firstQ)
+      examineeName: req.body.examineeName,
+      examineeAnswer: [],
+      score: 0
+
+    });
+    const firstQ = 1;
+    console.log("FIRSTQ : ", firstQ)
+    console.log("examineeName", insertdata.ops[0]._id)
+    // res.send("ok")
+    // alert("lkddhjskjsfdhjfh .........")
+    // res.send({})
+    // res.redirect('quiz/'+firstQ)
+    const query = querystring.stringify({
+      "firstQ": firstQ,
+      "examineeName": insertdata.ops[0].examineeName,
+      "examineeID": insertdata.ops[0]._id,
+    })
+    req.session.user = insertdata.ops[0].examineeName
+    req.session.userid = insertdata.ops[0]._id
+    res.redirect('quiz/' + firstQ)
+  }
+
 });
 
 // router.get('/start/:query',async function(req, res, next){
@@ -50,7 +60,7 @@ router.post('/register', async function(req, res, next){
 //   res.redirect('quiz/'+firstQ)
 // });
 
-router.get('/quiz/:pageNumber', async function(req, res, next){
+router.get('/quiz/:pageNumber', async function (req, res, next) {
   console.log("QUIZ PAGE CALLED.");
   const quiz = new Quiz();
   const nPerPage = 1;
@@ -69,42 +79,57 @@ router.get('/quiz/:pageNumber', async function(req, res, next){
 
     var quizDetails = await quiz.collection.find()
       .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
-      .limit(nPerPage) 
+      .limit(nPerPage)
       .toArray();
-    
+
     console.log("QuizDetails : ", quizDetails)
     return quizDetails;
   }
   let countTill = await quiz.collection.find().count();
-  let nextQ = parseInt(pageNumber)+1;
+  let nextQ = parseInt(pageNumber) + 1;
   console.log("NextQ : ", nextQ)
-  let sendData = {title : 'Quiz', items: await quizPage(pageNumber, nPerPage), nextQ: nextQ, examineeName: req.session.user};
+  let sendData = {
+    title: 'Quiz',
+    items: await quizPage(pageNumber, nPerPage),
+    nextQ: nextQ,
+    examineeName: req.session.user
+  };
   console.log("Send Data : ", sendData)
   res.render('quiz', sendData)
   console.log("session user id : ", req.session.userid)
 
 })
 
-router.post('/scoreAnswer', async function(req, res, next){
+router.post('/scoreAnswer', async function (req, res, next) {
   console.log("Score Answer API Called . . . .")
   const examinee = new Examinee();
   console.log("examinee Answer qID : ", req.body)
   let answerArray = [req.body.qID, req.body.answer]
-  await examinee.collection.updateOne({examineeName: req.session.user }, {$push : { examineeAnswer :  answerArray} })
-  .then(()=>{
-    console.log("Answer Updated")
-  })
-  
+  await examinee.collection.updateOne({
+      examineeName: req.session.user
+    }, {
+      $push: {
+        examineeAnswer: answerArray
+      }
+    })
+    .then(() => {
+      console.log("Answer Updated")
+    })
+
 })
 
-router.get("/alldata", async function(req,res, next){
+router.get("/alldata", async function (req, res, next) {
   const quiz = new Quiz();
-  var alldata = await quiz.collection.aggregate([{$sample: { size: 1 } }]).toArray();
+  var alldata = await quiz.collection.aggregate([{
+    $sample: {
+      size: 1
+    }
+  }]).toArray();
   console.log("All Data : ", alldata)
   res.send("OK")
 })
 
-router.get("/insertdata", async function(req,res, next){
+router.get("/insertdata", async function (req, res, next) {
   const quiz = new Quiz();
   // var insertdata = await quiz.collection.insertMany([
   //   {
